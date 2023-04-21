c887ebe88419dfae3fded3d4ea2522ca6a53e360
Hugo Fossé <fosse.hugo25@gmail.com>
2023-04-21 11:18:17 +0200

    Responsive + Contenu

--- js/source.js
+++ js/source.js
@@ -2,27 +2,22 @@ const nameForm = document.getElementById("name-form");
 const emailForm = document.getElementById("email-form");
 const subjectForm = document.getElementById("subject-form");
 const messageForm = document.getElementById("message-form");
-const errorMessage = document.getElementById("error-message");
-
+const submitMessage = document.getElementById("submit-message");
 const sendBtnForm = document.getElementById("btn-form");
-
-document.addEventListener("click", () => {
-    if(errorMessage.value) {
-        errorMessage.innerHTML = null;
-    }
-})
 
 sendBtnForm.addEventListener("click", (Event) => {
     let emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     Event.preventDefault();
 
     if(!nameForm.value || !emailForm.value || !messageForm.value || !subjectForm.value) { //Si un des champs est vide
-        errorMessage.innerHTML = "Veuillez remplir tout les champs"
+        submitMessage.style.color = "#FF0000";
+        submitMessage.innerHTML = "Veuillez remplir tout les champs"
     } else if(!emailFormat.test(emailForm.value)) {
-        errorMessage.innerHTML = "Veuillez rentrez une adresse mail valide"
+        submitMessage.style.color = "#FF0000";
+        submitMessage.innerHTML = "Veuillez rentrez une adresse mail valide"
     }else { //Formulaire OK
 
-        errorMessage.innerHTML = null;
+        submitMessage.innerHTML = null;
 
         const xhr = new XMLHttpRequest();
         const url = './serv/postmessage.php'
@@ -32,12 +27,14 @@ sendBtnForm.addEventListener("click", (Event) => {
         xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         xhr.onload = function() {
             if (xhr.status === 200) {
-            console.log(xhr.responseText); // Affiche la réponse du script PHP
+                nameForm.value=null;
+                emailForm.value=null;
+                messageForm.value=null;
+                subjectForm.value=null;
+                submitMessage.innerHTML = null;
 
-            nameForm.value=null;
-            emailForm.value=null;
-            messageForm.value=null;
-            subjectForm.value=null;
+                submitMessage.style.color = "#000000";
+                submitMessage.innerHTML = "Message envoyé."
             }
         };
         xhr.send(params);
